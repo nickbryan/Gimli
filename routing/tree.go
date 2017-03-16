45cdf4ccce31137e77b2b7dfd66aba73e31bc4d9
Nick Bryan <nick.bryan@mannisland.co.uk>
2017-03-16 14:02:00 +0000

    Refactoring tree

--- routing/tree.go
+++ routing/tree.go
@@ -4,6 +4,16 @@ import (
 	"strings"
 )
 
+// routeTree is defined to hide away the traverse method in the trie data structure
+// defined below.
+type routeTree interface {
+	add(route *Route)
+	search(path string) ([]*Route, map[string]string)
+}
+
+// tree is a simple trie data structure that allows for multiple routes to be added
+// to each node. The tree should be used to get all routes that match a path, any
+// matching on methods, regex, host, scheme etc should be handled elsewhere.
 type tree struct {
 	// Each node can have multiple children
 	// (ie. /some/path and /some/thing, "some" would have "path" and "thing" child node).
@@ -12,35 +22,39 @@ type tree struct {
 	// The part of the path (if path was /some/path, "some" and "path" would be a segment).
 	segment string
 
-	// Used to check if segment should be added to params when looking
-	// up a route.
+	// Used to check if segment should be added to params when looking up a route.
 	isNamedParam bool
 
-	// The route will be set on leaf nodes only.
-	route *Route
+	// Routes will be set on leaf nodes only.
+	routes []*Route
 }
 
-func newTree() *tree {
-	return &tree{segment: "/", isNamedParam: false, route: nil}
+// newRouteTree will return a new instance of a tree that implements the routeTree interface.
+// It also sets the first segment to / (this can be overridden by adding a route with "/" as the path.
+func newRouteTree() routeTree {
+	return &tree{segment: "/", isNamedParam: false, routes: nil}
 }
 
-func (t *tree) addRoute(route *Route) {
+// add will insert a new route into the trie.
+func (t *tree) add(route *Route) {
 	segments := strings.Split(route.Path(), "/")[1:]
 	numSegments := len(segments)
 
 	for {
-		node, segment, _ := t.traverse(segments)
+		// Traverse the segments to see if we match, if we don't then the original
+		// tree and segment is returned.
+		node, segment := t.traverse(segments, nil)
 
-		// If we get the leaf node again set the route and end.
-		if node.segment == segment {
-			node.route = route
+		// If we find a node that matches the last segment then update it and return.
+		if len(segments) > 0 && node.segment == segments[len(segments)-1] {
+			node.routes = append(node.routes, route)
 			return
 		}
 
 		newTreeNode := &tree{
 			segment:      segment,
 			isNamedParam: false,
-			route:        nil,
+			routes:       nil,
 		}
 
 		// Check for named route.
@@ -48,46 +62,61 @@ func (t *tree) addRoute(route *Route) {
 			newTreeNode.isNamedParam = true
 		}
 
-		// This is the last segment of the path so add the route.
-		if numSegments == 1 {
-			newTreeNode.route = route
+		// If the last node is the segment before the one we are adding then add to its child.
+		if len(segments) > 1 && node.segment == segments[len(segments)-2] {
+			newTreeNode.routes = append(newTreeNode.routes, route)
+			node.children = append(node.children, newTreeNode)
+			return
 		}
 
+		// This is the last segment of the path so add the route.
+		if numSegments == 1 {
+			newTreeNode.routes = append(newTreeNode.routes, route)
+		}
+
+		// Add a new child to the current node.
 		node.children = append(node.children, newTreeNode)
 		numSegments--
 
+		// Once we have gone through all the segments we are not updating
+		// and have added a node for every segment in the path.
 		if numSegments == 0 {
 			break
 		}
 	}
 }
 
-func (t *tree) traverse(segments []string) (*tree, string, map[string]string) {
-	segment := segments[0]
+// search will traverse the trie looking for a match to the path.
+// If nothing is found it will return (nil, map[string]string{}).
+func (t *tree) search(path string) ([]*Route, map[string]string) {
 	params := map[string]string{}
+
+	node, _ := t.traverse(strings.Split(path, "/")[1:], params)
+
+	return node.routes, params
+}
+
+// traverse recursively searches the trie based on the path segments and extracts
+// named params along the way.
+func (t *tree) traverse(segments []string, params map[string]string) (*tree, string) {
+	segment := segments[0]
 
 	if len(t.children) > 0 {
 		for _, child := range t.children {
 			if segment == child.segment || child.isNamedParam {
-				if child.isNamedParam {
+				if child.isNamedParam && params != nil {
 					params[child.segment[1:]] = segment
 				}
 
 				next := segments[1:]
 				if len(next) > 0 {
-					return child.traverse(next)
+					return child.traverse(next, params)
 				}
 
-				return child, segment, params
+				return child, segment
 			}
 		}
 	}
 
-	return t, segment, params
-}
-
-func (t *tree) findRoute(path string) (*Route, map[string]string) {
-	node, _, params := t.traverse(strings.Split(path, "/")[1:])
-
-	return node.route, params
+	return t, segment
 }
--- routing/tree_test.go
+++ routing/tree_test.go
@@ -6,27 +6,68 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNewRouteTree(t *testing.T) {
+	assert.Implements(t, (*routeTree)(nil), newRouteTree())
+}
+
 type routePair struct {
 	path  string
 	route *Route
 }
 
 var routePairs []*routePair = []*routePair{
+	{"/", NewRoute("/", nil)},
 	{"/path/to/route", NewRoute("/path/to/route", nil)},
 	{"/path/to/route/Nick", NewRoute("/path/to/route/:name", nil)},
 	{"/path/to/route/123/something", NewRoute("/path/to/route/:name/something", nil)},
+	{"/path/to/route/123/somethingelse", NewRoute("/path/to/route/:name/somethingelse", nil)},
 	{"/different", NewRoute("/different", nil)},
 	{"/different/path", NewRoute("/different/path", nil)},
 	{"/another/your-face/or/something/true", NewRoute("/another/:path/or/something/:else", nil)},
 }
 
-func TestRoutesCanBeMatchedByPath(t *testing.T) {
-	tr := newTree()
+func TestNilAndEmptyMapIsReturnedIfRouteNotFound(t *testing.T) {
+	tree := newRouteTree()
 
 	for _, routePair := range routePairs {
-		tr.addRoute(routePair.route)
-
-		rt, _ := tr.findRoute(routePair.path)
-		assert.Equal(t, routePair.route, rt)
+		tree.add(routePair.route)
 	}
+
+	routes, params := tree.search("/path/to")
+	assert.Nil(t, routes)
+	assert.Empty(t, params)
+}
+
+func TestRoutesCanBeMatchedByPath(t *testing.T) {
+	tree := newRouteTree()
+
+	for _, routePair := range routePairs {
+		tree.add(routePair.route)
+
+		routes, _ := tree.search(routePair.path)
+		assert.Equal(t, routePair.route, routes[0])
+	}
+}
+
+func TestMultipleRoutsCanBeAddedWithTheSamePath(t *testing.T) {
+	tree := newRouteTree()
+
+	route1 := NewRoute("/path/to/route", []string{"GET"})
+	route2 := NewRoute("/path/to/route", []string{"POST"})
+
+	tree.add(route1)
+	tree.add(route2)
+
+	routes, _ := tree.search("/path/to/route")
+	assert.Equal(t, []*Route{route1, route2}, routes)
+}
+
+func TestNamedParamsAreReturnedWhenRoutesAreFound(t *testing.T) {
+	tree := newRouteTree()
+
+	route := NewRoute("/path/to/route/:name/with/:id", []string{"GET"})
+	tree.add(route)
+
+	_, params := tree.search("/path/to/route/nick/with/123")
+	assert.Equal(t, map[string]string{"name": "nick", "id": "123"}, params)
 }
