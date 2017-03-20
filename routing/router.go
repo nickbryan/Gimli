3b040c78a35f27ded96c55c7e13549d56e7e6df6
Nick Bryan <nick.bryan@mannisland.co.uk>
2017-03-20 12:18:52 +0000

    Adding reminder for interfaces

--- routing/router.go
+++ routing/router.go
@@ -1,5 +1,10 @@
 package routing
 
+/*
+TODO: Maybe abstract interfaces for RouteAdder, RoutSearcher and RouteAdderSearcher. This can be used in place of trie
+for extension?
+*/
+
 type Router interface{}
 
 type router struct{}
