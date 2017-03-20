049fa888253ab4847869122e197ad8e81ed00bfe
Nick Bryan <nick.bryan@mannisland.co.uk>
2017-03-20 15:55:09 +0000

    Adding test for checking router implements http.Handler interface

--- routing/router.go
+++ routing/router.go
@@ -1,5 +1,7 @@
 package routing
 
+import "net/http"
+
 /*
 TODO: Maybe abstract interfaces for RouteAdder, RoutSearcher and RouteAdderSearcher. This can be used in place of trie
 for extension?
@@ -11,4 +13,8 @@ type router struct{}
 
 func NewRouter() Router {
 	return new(router)
+}
+
+func (r *router) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+
 }
--- routing/router_test.go
+++ routing/router_test.go
@@ -4,8 +4,13 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"net/http"
 )
 
-func TestNewContainer(t *testing.T) {
+func TestNewRouter(t *testing.T) {
 	assert.Implements(t, (*Router)(nil), NewRouter())
+}
+
+func TestRouterCanBePassedAsHttpHandler(t *testing.T) {
+	assert.Implements(t, (*http.Handler)(nil), NewRouter())
 }
